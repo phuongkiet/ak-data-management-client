95da50c7d1cac7d2e75935c2d2420d4f9dd729b8
phuongkiet <kietnp15092003@gmail.com>
2025-05-20 12:27:20 +0700

    [hotfix][FE][kietnp] #21. hotfix ProductTextArea

--- src/components/form/product-form/form-elements/product/ProductTextAreaInput.tsx
+++ src/components/form/product-form/form-elements/product/ProductTextAreaInput.tsx
@@ -14,7 +14,7 @@ export default function ProductTextAreaInput() {
           <ProductLabel>Description</ProductLabel>
           <ProductTextArea
             value={message}
-            onChange={(value) => setMessage(value)}
+            onChange={(e) => setMessage(e.target.value)}
             rows={6}
           />
         </div>
@@ -32,7 +32,7 @@ export default function ProductTextAreaInput() {
             rows={6}
             value={messageTwo}
             error
-            onChange={(value) => setMessageTwo(value)}
+            onChange={(e) => setMessageTwo(e.target.value)}
             hint="Please enter a valid message."
           />
         </div>
